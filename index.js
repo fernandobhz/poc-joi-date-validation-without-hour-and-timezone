12e7e4cf470d74d28bd8c2577d5d636db0d7b8c5
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-08-12 12:57:39 -0300

    feat: regex validation

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const Joi = require("joi");
 
-const data = {
-    birth_date: "1986-04-05T03:00:00.000Z"
-}
+// Joi date => validation has NOT worked for date-only
+console.log(
+  Joi.object({
+    birth_date: Joi.date().required(),
+  }).validate({
+    birth_date: "1986-04-05T03:00:00.000Z",
+  })
+);
 
-const jo = Joi.object({  
-  birth_date: Joi.date().required()  
-});
+// Joi regex => worked, validation has worked, invalid date-only input since it has time
+console.log(
+  Joi.object({
+    birth_date: Joi.string()
+      .regex(/^\d{4}-([0]\d|1[0-2])-([0-2]\d|3[01])$/)
+      .required(),
+  }).validate({
+    birth_date: "1986-04-05T03:00:00.000Z",
+  })
+);
 
-const { error, value } = jo.validate(data, {
-    abortEarly: false
-});
-
-if (error) console.error(error);
-else console.log(value);
+// Joi regex, validation has worked, valid date-only input
+console.log(
+  Joi.object({
+    birth_date: Joi.string()
+      .regex(/^\d{4}-([0]\d|1[0-2])-([0-2]\d|3[01])$/)
+      .required(),
+  }).validate({
+    birth_date: "1986-04-05",
+  })
+);
