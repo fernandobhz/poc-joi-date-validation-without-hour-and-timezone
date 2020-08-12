be3f357e97dcfb9a5cdb475202ac1bc7909147ee
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-08-12 14:12:06 -0300

    fix: code organization

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const Joi = require("joi");
 
-// Joi date => validation has NOT worked for date-only
-console.log(
+const log = (condition, { error, value }) => {
+  console.log();
+  console.log(condition);
+  if (error) console.log(error.message);
+  else console.log(value);
+};
+
+log(
+  "Joi date => see how joi handle date-only string with date() validation",
+  Joi.object({
+    birth_date: Joi.date().required(),
+  }).validate({
+    birth_date: "1986-04-05",
+  })
+);
+
+log(
+  "Joi date => validation has NOT worked for date-only",
   Joi.object({
     birth_date: Joi.date().required(),
   }).validate({
@@ -9,8 +25,8 @@ console.log(
   })
 );
 
-// Joi regex => worked, validation has worked, invalid date-only input since it has time
-console.log(
+log(
+  "Joi regex => worked, validation has worked, invalid date-only input since it has time",
   Joi.object({
     birth_date: Joi.string()
       .regex(/^\d{4}-([0]\d|1[0-2])-([0-2]\d|3[01])$/)
@@ -20,8 +36,8 @@ console.log(
   })
 );
 
-// Joi regex, validation has worked, valid date-only input
-console.log(
+log(
+  "Joi regex, validation has worked, valid date-only input",
   Joi.object({
     birth_date: Joi.string()
       .regex(/^\d{4}-([0]\d|1[0-2])-([0-2]\d|3[01])$/)
